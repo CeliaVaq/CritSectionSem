5dd3b57c332782f2da682a4aa387c8e17bbd8744
Celia <98520386+CeliaVaq@users.noreply.github.com>
2022-02-17 11:21:19 +0100

    Add files via upload

---
+++ BoundedSemaphore.py
@@ -0,0 +1,59 @@
+
+from multiprocessing import Process
+from multiprocessing import current_process
+from multiprocessing import Value, Array
+from multiprocessing import BoundedSemaphore
+
+N = 8
+
+
+def task(common, tid, critical, b):
+
+    a=0
+    for i in range(100):
+        print(f'{tid}−{i}: Non−critical Section')
+        a += 1
+        print(f'{tid}−{i}: End of non−critical Section')
+        critical[tid] = 1
+        
+        b.acquire()
+        try:
+            print(f'{tid}−{i}: Critical section')
+            v = common.value + 1
+            print(f'{tid}−{i}: Inside critical section')
+            common.value = v
+            print(f'{tid}−{i}: End of critical section')
+            critical[tid] = 0
+        finally:
+            b.release()
+
+def main():
+    lp = []
+    bounded = BoundedSemaphore(3)
+    common = Value('i', 0)
+    critical = Array('i', [0]*N)
+    for tid in range(N):
+        lp.append(Process(target=task, args=(common, tid, critical, bounded)))
+    print (f"Valor inicial del contador {common.value}")
+    for p in lp:
+        p.start()
+    
+    for p in lp:
+        p.join()
+    
+    print (f"Valor final del contador {common.value}")
+    print ("fin")
+
+if __name__ == "__main__":
+    main()
+"""
+    for num in range(10):
+        Process(target=task, args=(lock, num)).start()
+"""
+#lock -> semaforo binario con k = 1
+#BoundedSemaphore(k) -> semáforo arbitrario
+#l.acquire() es wait
+#l.release() es signal
+
+
+
